28ccce51b282226233a2c242e7c4d1d2d86f3b7d
danil_sx3d734 <danilcherep7@gmail.com>
2023-05-27 01:25:11 +0600

    first-version

--- src/main/java/org/cherepanov/dto/RegistrationDto.java
+++ src/main/java/org/cherepanov/dto/RegistrationDto.java
@@ -2,8 +2,10 @@ package org.cherepanov.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class RegistrationDto {
+public class RegistrationDto implements Serializable {
 
     private String name;
 
@@ -12,4 +14,38 @@ public class RegistrationDto {
     private String phone;
 
     private String email;
+
+    public byte[] getPassword() {
+        // Преобразование char[] в byte[]
+        if (password != null) {
+            byte[] passwordBytes = new byte[password.length];
+            for (int i = 0; i < password.length; i++) {
+                passwordBytes[i] = (byte) password[i];
+            }
+            return passwordBytes;
+        }
+        throw new NullPointerException("Поле пароля пусто");
+    }
+
+    public void setPassword(byte[] password) {
+        // Преобразование byte[] в char[]
+        if (password != null) {
+            char[] passwordChars = new char[password.length];
+            for (int i = 0; i < password.length; i++) {
+                passwordChars[i] = (char) password[i];
+            }
+            this.password = passwordChars;
+        } else {
+            this.password = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "RegistrationDto{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
